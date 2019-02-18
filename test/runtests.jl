d25a86c52272a94c13c20f35297621d698e21d28
MLackner <lacknersmichael@gmail.com>
2019-02-18 16:48:07 +0100

    Fixes for 1.0 + further test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,14 @@ using AndorSIF
 using Test
 import FileIO: load
 
-testfile = joinpath(@__DIR__, "testfiles/2d_testfile.sif")
-@test load(testfile)
+testfile_2d = joinpath(@__DIR__, "testfiles/2d_testfile.sif")
+testfile_1d = joinpath(@__DIR__, "testfiles/1d_testfile.sif")
+
+img_2d = load(testfile_2d)
+img_1d = load(testfile_1d)
+
+@test size(img_2d) == (512, 512)
+@test size(img_1d) == (1, 512)
+@test img_1d.data[1:2] == [789.0, 783.0]
+@test img_1d.data[end-1:end] == [765.0, 768.0]
+@test img_1d.properties["ixon"]["exposure_time"] == 5.0
